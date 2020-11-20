457d6a38cf6e53e188087a6dcfad715a4d86a045
Harry-George <harry.m.george@gmail.com>
2020-11-20 20:08:24 +0000

    Clean up interfaces

--- src/com/f1updates/Main.java
+++ src/com/f1updates/Main.java
@@ -1,54 +1,98 @@
 package com.f1updates;
+
 import java.io.*;
 import java.net.*;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.util.Optional;
 
+
+interface IReader {
+    public ByteBuffer Get() throws Exception;
+}
+
+class FileReader implements IReader {
+    public FileReader() throws Exception {
+        this.inputStream_ = new FileInputStream("C:\\Users\\Hobby\\IdeaProjects\\F1Updates\\data\\f1.output");
+    }
+
+    public ByteBuffer Get() throws Exception {
+        byte[] badger = new byte[4];
+        if (0 == inputStream_.read(badger, 0, 4)) {
+            // todo - this better
+            throw new Exception("Finished");
+        }
+        int len = ByteBuffer.wrap(badger).getInt();
+        if (len == 0) {
+            throw new Exception("Finished 2");
+        }
+
+        receiveData_ = new byte[len];
+        if (len != inputStream_.read(receiveData_, 0, len)) {
+            throw new Exception("Unable to parse file");
+        }
+        return ByteBuffer.wrap(receiveData_, 0, len);
+    }
+
+    byte[] receiveData_;
+    InputStream inputStream_;
+}
+
+class PacketReader implements IReader {
+    public PacketReader(int port) throws Exception {
+        System.out.printf("Listening on udp:%s:%d%n",
+                InetAddress.getLocalHost().getHostAddress(), port);
+        this.serverSocket_ = new DatagramSocket(port);
+        this.receivePacket_ = new DatagramPacket(buffer_,
+                buffer_.length);
+    }
+
+    public ByteBuffer Get() throws Exception {
+
+        serverSocket_.receive(receivePacket_);
+        return ByteBuffer.wrap(receivePacket_.getData(), 0, receivePacket_.getLength());
+    }
+
+    DatagramSocket serverSocket_;
+    DatagramPacket receivePacket_;
+    byte[] buffer_ = new byte[2000];
+}
 
 public class Main {
 
     public static void main(String[] args) {
         int port = args.length == 0 ? 20777 : Integer.parseInt(args[0]);
+        // TODO - arg parsing
+        boolean readFromFile = true;
+
+        IReader reader;
+
+        int rowsRead = 0;
         try {
-            DatagramSocket serverSocket = new DatagramSocket(port);
-            byte[] receiveData = new byte[500];
+            if (readFromFile) {
+                reader = new FileReader();
+            } else {
+                reader = new PacketReader(port);
+            }
 
-            System.out.printf("Listening on udp:%s:%d%n",
-                    InetAddress.getLocalHost().getHostAddress(), port);
-            DatagramPacket receivePacket = new DatagramPacket(receiveData,
-                    receiveData.length);
-            InputStream inputStream = new FileInputStream("C:\\Users\\Hobby\\IdeaProjects\\F1Updates\\data\\f1.output");
-            while(true)
-            {
 
-//                serverSocket.receive(receivePacket);
-
-                byte[] badger = new byte[4];
-                if (inputStream.read(badger,0,4) == 0) {
-                    return;
-                }
-                int len = ByteBuffer.wrap(badger).getInt();
-                if (len == 0) {
-                    return;
-                }
-                receiveData = new byte[len];
-                System.out.println("Read bytes:" + len);
-                inputStream.read(receiveData,0,len);
-                 Optional<PacketHeader> header = PacketHeader.ParsePacketHeader(ByteBuffer.wrap(receiveData, 0, len));
+            while (true) {
+                rowsRead += 1;
+                ByteBuffer buffer = reader.Get();
+                Optional<PacketHeader> header = PacketHeader.ParsePacketHeader(buffer);
                 if (header == null) {
                     System.out.println("Failed to parse");
+                    continue;
                 } else {
                     System.out.println(header.get().packetId.toString());
                 }
-//                Object weatherForecast = WeatherForecast.ParseWeather(ByteBuffer.allocate(20));
-//                System.out.println(weatherForecast);
-
             }
         } catch (IOException e) {
             System.out.println(e);
+        } catch (Exception e) {
+            System.out.println(e);
         }
+        System.out.println("Read " + rowsRead + " rows.");
     }
 
     public void run(int port) {
--- src/com/f1writer/Main.java
+++ src/com/f1writer/Main.java
@@ -1,6 +1,8 @@
 package com.f1writer;
 
 import com.f1updates.PacketHeader;
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
 
 import java.io.*;
 import java.net.DatagramPacket;
@@ -8,15 +10,25 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.nio.ByteBuffer;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Main {
 
     public static void main(String[] args) {
 
         int port = args.length == 0 ? 20777 : Integer.parseInt(args[0]);
+        int rowsWritten = 0;
+
         try {
+            AtomicBoolean keepGoing = new AtomicBoolean(true);
+            SignalHandler handler = sig -> {
+                System.out.println("captured signal " + sig);
+                keepGoing.set(false);
+            };
+            Signal.handle(new Signal("INT"), handler);
+
             DatagramSocket serverSocket = new DatagramSocket(port);
-             // Hopefully this is big enough for any packet
+            // Hopefully this is big enough for any packet
             byte[] receiveData = new byte[10000];
 
             System.out.printf("Listening on udp:%s:%d%n",
@@ -25,8 +37,9 @@ public class Main {
                     receiveData.length);
             // TODO - make it so we can pass this in
             OutputStream outputStream = new FileOutputStream("C:\\Users\\Hobby\\IdeaProjects\\F1Updates\\data\\f1.output");
-            // TODO - make it so we can cleanly exit
-            while (true) {
+            while (keepGoing.get()) {
+
+                rowsWritten++;
 
                 serverSocket.receive(receivePacket);
 
@@ -38,6 +51,8 @@ public class Main {
         } catch (IOException e) {
             System.out.println(e);
         }
+
+        System.out.println("Wrote " + rowsWritten + " rows.");
     }
 
     public void run(int port) {
